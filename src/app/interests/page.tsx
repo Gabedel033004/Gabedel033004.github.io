7954223aea58de87c1b42c9fe76b06bb13a289c6
Gabe Delisle <133575614+Gabedel033004@users.noreply.github.com>
2026-05-25 17:03:38 -0600

    Fix icon class for Hockey interest

--- src/app/interests/page.tsx
+++ src/app/interests/page.tsx
@@ -1,7 +1,7 @@
 import styles from './page.module.css';
 
 const interests = [
-  { icon: 'ti-hockey-stick',        title: 'Hockey',    desc: 'Montreal Canadiens die hard fan' },
+  { icon: 'ti-hockey',        title: 'Hockey',    desc: 'Montreal Canadiens die hard fan' },
   { icon: 'ti-mountain',            title: 'Travelling',   desc: 'Montreal will always be my favourite city' },
   { icon: 'ti-microphone',title: 'Concerts',  desc: 'Everything from Rihanna to Luke Combs' },
   { icon: 'ti-music',           title: 'Music',     desc: 'Hip hop, Country, 2000s anthems' },
