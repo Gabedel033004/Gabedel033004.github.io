75b96b00ce09f587ca29c06f21e5f3c095a60ef2
Gabe Delisle <133575614+Gabedel033004@users.noreply.github.com>
2026-05-25 17:51:42 -0600

    Update page.tsx

--- src/app/personal/page.tsx
+++ src/app/personal/page.tsx
@@ -12,7 +12,7 @@ const values = [
 ];
 
 const interests = [
-  { icon: 'ti-hockey-stick', title: 'Hockey',    desc: 'Montreal Canadiens die hard fan' },
+  { icon: 'ti-ice-skating', title: 'Hockey',    desc: 'Montreal Canadiens die hard fan' },
   { icon: 'ti-mountain',     title: 'Travelling', desc: 'Montreal will always be my favourite city' },
   { icon: 'ti-microphone',   title: 'Concerts',   desc: 'Everything from Rihanna to Luke Combs' },
   { icon: 'ti-music',        title: 'Music',      desc: 'Hip hop, Country, 2000s anthems' },
